121ce5b1ecb1825d565475a7b40352409d8ac9b3
Medill-East <NEMailLHD@163.com>
2018-09-19 16:59:54 +0800

    A.3 构造方法
    201809191659

---
+++ Coding/A/A.3 构造方法/A.2 动物运动会/A.2 动物运动会/Cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A._2_动物运动会
+{
+    class Cat
+    {
+        private string name = "";
+
+        public Cat(string inputName)
+        {
+            this.name = inputName;
+        }
+
+        public string Shout()
+        {
+            return "我的名字叫 " + name + " 喵~";
+        }
+    }
+}
---
+++ Coding/A/A.3 构造方法/A.2 动物运动会/A.2 动物运动会/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A._2_动物运动会
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 猫叫_Click(object sender, EventArgs e)
+        {
+            Cat cat = new Cat("nekopara");
+            MessageBox.Show(cat.Shout());
+
+        }
+    }
+}
