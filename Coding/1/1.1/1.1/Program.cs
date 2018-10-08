340cb6433171025e8e406cec1e3239f07891a6f0
Medill-East <NEMailLHD@163.com>
2018-10-08 23:38:16 +0800

    1.1 面试受挫
    201810082338

---
+++ Coding/1/1.1/1.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("请输入数字A：");
+            string A = Console.ReadLine();
+            Console.Write("请选择运算符号(+、-、*、/):");
+            string B = Console.ReadLine();
+            Console.Write("请输入数字B：");
+            string C = Console.ReadLine();
+            string D = "";
+
+            if(B == "+")
+            {
+                D = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(C));
+            }
+            if (B == "-")
+            {
+                D = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(C));
+            }
+            if (B == "*")
+            {
+                D = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(C));
+            }
+            if (B == "/")
+            {
+                D = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(C));
+            }
+
+            Console.WriteLine("结果是：" + D);
+        }
+    }
+}
