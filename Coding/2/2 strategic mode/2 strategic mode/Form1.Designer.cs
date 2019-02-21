7575cff8d3830d0673f5c38a1b328a2985bb56f2
Medill-East <NEMailLHD@163.com>
2019-02-21 22:24:56 +0800

    designPattern 2.1
    201902212224

---
+++ Coding/2/2 strategic mode/2 strategic mode/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace _2_strategic_mode
+{
+    partial class 商场收银系统
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbxList = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "单价：";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(116, 60);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 25);
+            this.txtPrice.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "数量：";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(116, 111);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 25);
+            this.txtNum.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(222, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "重置";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lbxList
+            // 
+            this.lbxList.FormattingEnabled = true;
+            this.lbxList.ItemHeight = 15;
+            this.lbxList.Location = new System.Drawing.Point(61, 162);
+            this.lbxList.Name = "lbxList";
+            this.lbxList.Size = new System.Drawing.Size(249, 109);
+            this.lbxList.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "总计：";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblResult.Location = new System.Drawing.Point(148, 308);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(83, 34);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Text = "0.00";
+            // 
+            // 商场收银系统
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbxList);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label1);
+            this.Name = "商场收银系统";
+            this.Text = "商场收银系统";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox lbxList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Coding/2/2 strategic mode/2 strategic mode/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_strategic_mode
+{
+    public partial class 商场收银系统 : Form
+    {
+        double total = 0.0d;
+
+        public 商场收银系统()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double totalPrice = Convert.ToDouble(txtPrice.Text) * Convert.ToDouble(txtNum.Text);
+
+            total = total + totalPrice;
+
+            lbxList.Items.Add("单价： " + txtPrice.Text + " 数量： "
+                + txtNum.Text + " 合计： " + totalPrice.ToString());
+            lblResult.Text = total.ToString();
+        }
+    }
+}
