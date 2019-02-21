386a05be1585849303963e6cc5f98e8d4b8fc8e2
Medill-East <NEMailLHD@163.com>
2019-02-21 22:57:24 +0800

    designPattern 2.2
    201902202257

--- Coding/2/2 strategic mode/2 strategic mode/Form1.Designer.cs
+++ Coding/2/2 strategic mode/2 strategic mode/Form1.Designer.cs
@@ -37,12 +37,14 @@
             this.lbxList = new System.Windows.Forms.ListBox();
             this.label3 = new System.Windows.Forms.Label();
             this.lblResult = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbxType = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(58, 63);
+            this.label1.Location = new System.Drawing.Point(58, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(52, 15);
             this.label1.TabIndex = 0;
@@ -50,7 +52,7 @@
             // 
             // txtPrice
             // 
-            this.txtPrice.Location = new System.Drawing.Point(116, 60);
+            this.txtPrice.Location = new System.Drawing.Point(116, 25);
             this.txtPrice.Name = "txtPrice";
             this.txtPrice.Size = new System.Drawing.Size(100, 25);
             this.txtPrice.TabIndex = 1;
@@ -58,7 +60,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(58, 114);
+            this.label2.Location = new System.Drawing.Point(58, 76);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 15);
             this.label2.TabIndex = 2;
@@ -66,14 +68,14 @@
             // 
             // txtNum
             // 
-            this.txtNum.Location = new System.Drawing.Point(116, 111);
+            this.txtNum.Location = new System.Drawing.Point(116, 73);
             this.txtNum.Name = "txtNum";
             this.txtNum.Size = new System.Drawing.Size(100, 25);
             this.txtNum.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(222, 50);
+            this.button1.Location = new System.Drawing.Point(222, 13);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 45);
             this.button1.TabIndex = 4;
@@ -83,7 +85,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(222, 111);
+            this.button2.Location = new System.Drawing.Point(222, 64);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(88, 45);
             this.button2.TabIndex = 5;
@@ -96,7 +98,7 @@
             this.lbxList.ItemHeight = 15;
             this.lbxList.Location = new System.Drawing.Point(61, 162);
             this.lbxList.Name = "lbxList";
-            this.lbxList.Size = new System.Drawing.Size(249, 109);
+            this.lbxList.Size = new System.Drawing.Size(382, 109);
             this.lbxList.TabIndex = 6;
             // 
             // label3
@@ -118,11 +120,30 @@
             this.lblResult.TabIndex = 8;
             this.lblResult.Text = "0.00";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "计算方式：";
+            // 
+            // cbxType
+            // 
+            this.cbxType.FormattingEnabled = true;
+            this.cbxType.Location = new System.Drawing.Point(146, 126);
+            this.cbxType.Name = "cbxType";
+            this.cbxType.Size = new System.Drawing.Size(121, 23);
+            this.cbxType.TabIndex = 11;
+            // 
             // 商场收银系统
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbxType);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lbxList);
@@ -134,6 +155,7 @@
             this.Controls.Add(this.label1);
             this.Name = "商场收银系统";
             this.Text = "商场收银系统";
+            this.Load += new System.EventHandler(this.商场收银系统_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,6 +172,8 @@
         private System.Windows.Forms.ListBox lbxList;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbxType;
     }
 }
 
--- Coding/2/2 strategic mode/2 strategic mode/Form1.cs
+++ Coding/2/2 strategic mode/2 strategic mode/Form1.cs
@@ -19,15 +19,43 @@ namespace _2_strategic_mode
             InitializeComponent();
         }
 
+        private void 商场收银系统_Load(object sender, EventArgs e)
+        {
+            cbxType.Items.AddRange(new object[] { "正常收费","打八折","打七折","打五折" });
+            cbxType.SelectedIndex = 0;
+
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            double totalPrice = Convert.ToDouble(txtPrice.Text) * Convert.ToDouble(txtNum.Text);
+            double totalPrices = 0d;
+            switch (cbxType.SelectedIndex)
+            {
+                case 0:
+                    totalPrices = Convert.ToDouble(txtPrice.Text) * Convert.ToDouble(txtNum.Text);
+                    break;
+                case 1:
+                    totalPrices = Convert.ToDouble(txtPrice.Text) * Convert.ToDouble(txtNum.Text) * 0.8;
+                    break;
+                case 2:
+                    totalPrices = Convert.ToDouble(txtPrice.Text) * Convert.ToDouble(txtNum.Text) * 0.7;
+                    break;
+                case 3:
+                    totalPrices = Convert.ToDouble(txtPrice.Text) * Convert.ToDouble(txtNum.Text) * 0.5;
+                    break;
+                default:
+                    break;
+            }
 
-            total = total + totalPrice;
+            //double totalPrice = Convert.ToDouble(txtPrice.Text) * Convert.ToDouble(txtNum.Text);
+
+            total = total + totalPrices;
 
             lbxList.Items.Add("单价： " + txtPrice.Text + " 数量： "
-                + txtNum.Text + " 合计： " + totalPrice.ToString());
+                + txtNum.Text + " 合计： " + totalPrices.ToString());
             lblResult.Text = total.ToString();
         }
+
+
     }
 }
